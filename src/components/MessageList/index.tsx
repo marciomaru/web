e5c3cdba9d03c14ab9e552e8b756b716e2b2f954
marcio <marciosousar@hotmail.com>
2021-10-23 00:27:15 -0300

    conexão front com back ok!

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,49 +1,52 @@
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api'
+
 import styles from './styles.module.scss';
 
 import logoImg from '../../assets/logo.svg';
 
+type Message = {
+    id: string;
+    text: string;
+    user: {
+        name: string;
+        avatar_url: string
+    }
+}
+
 export function MessageList() {
+    const [message, setMessage] = useState<Message[]>([])
+
+    useEffect(() => {
+        api.get<Message[]>('messages/last3').then(response => {
+            setMessage(response.data);
+        });
+    }, []);
+
     return (
         <div className={styles.messageListWrapper}>
             <img src={logoImg} alt="DoWhile 2021" />
 
             <ul className={styles.messageList}>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>
-                        dlkfjaldkfjalkdfjalkjdfalkjdfhalkjfhalkjdhflakjhflakjfhlakjdfhla adflkjadlfkjhadlkfhakjldf alkdjfhalkjdfhda flakjfhaldskfjha
-                    </p>
+                {message.map(message => {
+                    return (
+                        <li key={message.id} className={styles.message}>
+                            <p className={styles.messageContent}>
+                                {message.text}
+                            </p>
 
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/marciomaru.png" alt="Marcio Sousa" />
-                        </div>
-                        <span>Marcio Sousa</span>
-                    </div>
-                </li>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>
-                        dlkfjaldkfjalkdfjalkjdfalkjdfhalkjfhalkjdhflakjhflakjfhlakjdfhla adflkjadlfkjhadlkfhakjldf alkdjfhalkjdfhda flakjfhaldskfjha
-                    </p>
+                            <div className={styles.messageUser}>
+                                <div className={styles.userImage}>
+                                    <img src={message.user.avatar_url} alt={message.user.name} />
+                                </div>
+                                <span>{message.user.name}</span>
+                            </div>
+                        </li>
 
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/marciomaru.png" alt="Marcio Sousa" />
-                        </div>
-                        <span>Marcio Sousa</span>
-                    </div>
-                </li>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>
-                        dlkfjaldkfjalkdfjalkjdfalkjdfhalkjfhalkjdhflakjhflakjfhlakjdfhla adflkjadlfkjhadlkfhakjldf alkdjfhalkjdfhda flakjfhaldskfjha
-                    </p>
+                    );
+                })}
 
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/marciomaru.png" alt="Marcio Sousa" />
-                        </div>
-                        <span>Marcio Sousa</span>
-                    </div>
-                </li>
+
             </ul>
         </div>
     )
